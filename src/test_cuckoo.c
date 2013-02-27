5d48122a6a300e04f65d66014f7e94fd5fad15a9
Bin Fan <binfan@cs.cmu.edu>
2013-02-26 20:08:30 -0500

    update the test file to have two cuckoo hash tables

--- src/test_cuckoo.c
+++ src/test_cuckoo.c
@@ -14,57 +14,71 @@ int main(int argc, char** argv)
 {
 
     int i;    
-    size_t power = 20;
+    size_t power = 19;
     size_t numkeys = (1 << power) * 4;
-    cuckoo_hashtable_t* h;
+    cuckoo_hashtable_t* smalltable;
+    cuckoo_hashtable_t* bigtable;
 
-
-    printf("initializing a hash table\n");
-    h = cuckoo_init(power);
+    printf("initializing two hash table\n");
+    smalltable = cuckoo_init(power);
+    bigtable   = cuckoo_init(power + 1);
 
     printf("inserting keys to the hash table\n");
+    int failure = -1;
     for (i = 1; i < numkeys; i ++) {
         KeyType key;
         ValType val;
+        cuckoo_status st;
         key = (KeyType) i;
         val = (ValType) i * 2 - 1;
-        cuckoo_status st = cuckoo_insert(h, (const char*) &key, (const char*) &val);
-
+        if (failure == -1) {
+            st = cuckoo_insert(smalltable, (const char*) &key, (const char*) &val);
+            if (st != ok) {
+                printf("inserting key %d to smalltable fails \n", i);
+                failure = i;
+            }
+        }
+        st = cuckoo_insert(bigtable, (const char*) &key, (const char*) &val);
         if (st != ok) {
-             printf("insert failure at key %d\n", i);
-             break;
+            printf("inserting key %d to bigtable fails \n", i);
+            break;
         }
     }
 
-    int failure = i;
+
+
+
 
     printf("looking up keys in the hash table\n");
     for (i = 1; i < numkeys; i ++) {
         KeyType key;
-        ValType val;
+        ValType val1, val2;
         key = (KeyType) i;
-        cuckoo_status st = cuckoo_find(h, (const char*) &key, (char*) &val);
+        cuckoo_status st1 = cuckoo_find(smalltable, (const char*) &key, (char*) &val1);
+        cuckoo_status st2 = cuckoo_find(bigtable, (const char*) &key, (char*) &val2);
         if (i < failure) {
-            if (st != ok) {
-                printf("failure to read key %d\n", i);
+            if (st1 != ok) {
+                printf("failure to read key %d from smalltable\n", i);
                 break;
             }
-            if (val != 2 * i - 1) {
-                printf("wrong value for key %d\n", i);
+            if (val1 != val2) {
+                printf("smalltable and bigtable disagree on key %d\n", i);
                 break;
             }
         }
         else {
-            if (st != not_found) {
-                printf("found key %d, which should not be in the table\n", i);
+            if (st1 != not_found) {
+                printf(" key %d should not be in smalltable\n", i);
                 break;
             }
         }
 
     }
 
-    cuckoo_report(h);
+    cuckoo_report(smalltable);
+    cuckoo_exit(smalltable);
 
-    cuckoo_exit(h);
+    cuckoo_report(bigtable);
+    cuckoo_exit(bigtable);
 
 }
