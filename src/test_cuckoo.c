9406d46020ee49e3cfce397d5e026d11aee9edff
Dave Andersen <dga@cs.cmu.edu>
2013-02-27 16:50:06 -0500

    Cleaning up test code - adding explicit passed or failed indicator;  merging some variable declaration and initializations.

--- src/test_cuckoo.c
+++ src/test_cuckoo.c
@@ -7,30 +7,27 @@
 #include <pthread.h>
 #include <math.h>
 
-
 #include "cuckoohash.h"
 
 int main(int argc, char** argv) 
 {
-
+    bool passed = true;
     int i;    
     size_t power = 19;
     size_t numkeys = (1 << power) * 4;
-    cuckoo_hashtable_t* smalltable;
-    cuckoo_hashtable_t* bigtable;
 
-    printf("initializing two hash table\n");
-    smalltable = cuckoo_init(power);
-    bigtable   = cuckoo_init(power + 1);
+    printf("initializing two hash tables\n");
+
+    cuckoo_hashtable_t* smalltable = cuckoo_init(power);
+    cuckoo_hashtable_t* bigtable = cuckoo_init(power + 1);
 
     printf("inserting keys to the hash table\n");
     int failure = -1;
     for (i = 1; i < numkeys; i ++) {
-        KeyType key;
-        ValType val;
+        KeyType key = (KeyType) i;
+        ValType val = (ValType) i * 2 - 1;
         cuckoo_status st;
-        key = (KeyType) i;
-        val = (ValType) i * 2 - 1;
+
         if (failure == -1) {
             st = cuckoo_insert(smalltable, (const char*) &key, (const char*) &val);
             if (st != ok) {
@@ -46,33 +43,33 @@ int main(int argc, char** argv)
     }
 
 
-
-
-
     printf("looking up keys in the hash table\n");
     for (i = 1; i < numkeys; i ++) {
-        KeyType key;
         ValType val1, val2;
-        key = (KeyType) i;
+        KeyType key = (KeyType) i;
         cuckoo_status st1 = cuckoo_find(smalltable, (const char*) &key, (char*) &val1);
         cuckoo_status st2 = cuckoo_find(bigtable, (const char*) &key, (char*) &val2);
         if (i < failure) {
             if (st1 != ok) {
                 printf("failure to read key %d from smalltable\n", i);
+		passed = false;
                 break;
             }
             if (st2 != ok) {
                 printf("failure to read key %d from bigtable\n", i);
+		passed = false;
                 break;
             }
             if (val1 != val2) {
                 printf("smalltable and bigtable disagree on key %d\n", i);
+		passed = false;
                 break;
             }
         }
         else {
             if (st1 != not_found) {
                 printf(" key %d should not be in smalltable\n", i);
+		passed = false;
                 break;
             }
         }
@@ -84,6 +81,8 @@ int main(int argc, char** argv)
 
     cuckoo_report(bigtable);
     cuckoo_exit(bigtable);
+
+    printf("[%s]\n", passed ? "PASSED" : "FAILED");
 
     return 0;
 }
